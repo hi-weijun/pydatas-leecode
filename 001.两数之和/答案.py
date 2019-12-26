ba9959c4dc51cff93561fe7ac546a5886d9f2b08
Amos Albert <47343901+Albertchamberlain@users.noreply.github.com>
2019-12-26 10:16:07 +0800

    支持python3.7

---
+++ 001.两数之和/答案.py
@@ -0,0 +1,12 @@
+class Solution(object):
+  def twoSum(self, nums, target):
+    """
+    :nums类型: List[int]
+    :目标类型: int
+    """
+    d = {}
+    for i, num in enumerate(nums):
+      if target - num in d:
+        return [d[target - num], i]
+      d[num] = i
+    # 没有特殊情况处理,因为它假定只有一个解决方案
