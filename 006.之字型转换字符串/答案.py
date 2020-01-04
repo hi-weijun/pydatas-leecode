1922cd4f2f08e268827a83be2d392a02ffad1f3c
Albertchamberlain <2476271447@qq.com>
2020-01-04 13:49:45 +0800

    <feat>:<第006与第007题>

---
+++ 006.之字型转换字符串/答案.py
@@ -0,0 +1,23 @@
+class Solution(object):
+  def convert(self, s, numRows):
+    """
+    :s的类型: str
+    :numRows类型: int
+    :返回类型: str
+    """
+    if numRows <= 1:
+      return s
+    n = len(s)
+    ans = []
+    step = 2 * numRows - 2
+    for i in range(numRows):
+      one = i
+      two = -i
+      while one < n or two < n:
+        if 0 <= two < n and one != two and i != numRows - 1:
+          ans.append(s[two])
+        if one < n:
+          ans.append(s[one])
+        one += step
+        two += step
+    return "".join(ans)
