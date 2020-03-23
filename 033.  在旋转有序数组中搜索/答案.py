8f7d341b3bdd34dc1daa27190d2d27a065862f78
Albertchamberlain <2476271447@qq.com>
2020-03-23 21:26:03 +0800

    <feat>:<第33题>

---
+++ 033.  在旋转有序数组中搜索/答案.py
@@ -0,0 +1,23 @@
+# 二分搜索
+class Solution(object):
+  def search(self, nums, target):
+
+    if not nums:
+      return -1
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+      mid = (right + left) / 2
+      if nums[mid] == target:
+        return mid
+      if nums[mid] >= nums[left]:
+        if nums[left] <= target <= nums[mid]:
+          right = mid - 1
+        else:
+          left = mid + 1
+      else:
+        if nums[mid] <= target <= nums[right]:
+          left = mid + 1
+        else:
+          right = mid - 1
+    return -1
