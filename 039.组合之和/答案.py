6bb45b6d7d6e6e7bf8bd715c6c3fbd491418842c
Albertchamberlain <2476271447@qq.com>
2020-05-02 17:02:54 +0800

    <feat>:<第39题>

---
+++ 039.组合之和/答案.py
@@ -0,0 +1,16 @@
+class Solution(object):
+  def combinationSum(self, candidates, target):
+
+    def dfs(candidates, start, target, path, res):
+      if target == 0:
+        return res.append(path + [])
+
+      for i in range(start, len(candidates)):
+        if target - candidates[i] >= 0:
+          path.append(candidates[i])
+          dfs(candidates, i, target - candidates[i], path, res)
+          path.pop()
+
+    res = []
+    dfs(candidates, 0, target, [], res)
+    return res
