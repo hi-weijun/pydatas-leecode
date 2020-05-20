dd917da459b67e28d71ac9c9fd84e3c2b1a9b4d7
Albertchamberlain <2476271447@qq.com>
2020-05-20 16:59:34 +0800

    <feat>:<第41题>

---
+++ 041首个未出现的正数/答案.py
@@ -0,0 +1,13 @@
+class Solution(object):
+  def firstMissingPositive(self, nums):
+    i = 0
+    while i < len(nums):
+      if 0 < nums[i] <= len(nums) and nums[nums[i] - 1] != nums[i]:
+        nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+      else:
+        i += 1
+
+    for i in range(0, len(nums)):
+      if nums[i] != i + 1:
+        return i + 1
+    return len(nums) + 1
